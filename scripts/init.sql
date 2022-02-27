2885b834014e72a5548ec5f91a71fb4f000bdc6a
Sergio Rodríguez <sergio.alepuz@observe.tech>
2022-02-27 16:56:40 +0100

    chore: defines init DDL for Docker databases

---
+++ scripts/init.sql
@@ -0,0 +1,19 @@
+CREATE TABLE tasks (
+	id serial PRIMARY KEY,
+	title VARCHAR (255) NOT NULL,
+	status VARCHAR (255) NOT NULL,
+	updated_at TIMESTAMP NOT NULL
+);
+
+CREATE TABLE tasks_lists (
+	id serial PRIMARY KEY,
+	name VARCHAR (255) NOT NULL,
+	updated_at TIMESTAMP NOT NULL
+);
+
+CREATE TABLE tasks__tasks_lists (
+  task_id INT REFERENCES tasks(id) ON DELETE CASCADE ON UPDATE CASCADE,
+  task_list_id INT REFERENCES tasks_lists(id) ON DELETE CASCADE ON UPDATE CASCADE,
+	assigned_at TIMESTAMP NOT NULL,
+  PRIMARY KEY (task_id, task_list_id)
+);
