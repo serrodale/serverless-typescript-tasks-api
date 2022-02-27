d53a5d7f6e4640fe6914d58c8da654d5e371ab3b
Sergio Rodríguez <sergio.alepuz@observe.tech>
2022-02-27 11:28:53 +0100

    chore: scaffolding of Serverless project

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+	
+import express from 'express';
+import serverless from 'serverless-http';
+import { defineRoutes } from './routes';
+import { defineMiddlewares } from './middlewares';
+
+export const app = express();
+
+defineRoutes(app);
+defineMiddlewares(app);
+ 
+export const handler = serverless(app);
